0b90485f34a58fe90eed9231ba3d711755acd555
Jasmin <jasmin.merusic@gmail.com>
2018-10-17 16:05:25 +0200

    Impl Map based Services Closes #6.

---
+++ pet-clinic-data/src/main/java/com/jasko/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package com.jasko.sfgpetclinic.services.map;
+
+import com.jasko.sfgpetclinic.model.Owner;
+import com.jasko.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.finfAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+      super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
