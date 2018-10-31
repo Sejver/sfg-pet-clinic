78ca1e4ed41f40f29a75ea3af96bfa27502ce7d9
Jasmin <jasmin.merusic@gmail.com>
2018-10-31 19:41:43 +0100

    Vet specialities and added to Vet. closes #26

--- pet-clinic-data/src/main/java/com/jasko/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jasko/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.jasko.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialies;
+
+    public Set<Speciality> getSpecialies() {
+        return specialies;
+    }
+
+    public void setSpecialies(Set<Speciality> specialies) {
+        this.specialies = specialies;
+    }
 }
