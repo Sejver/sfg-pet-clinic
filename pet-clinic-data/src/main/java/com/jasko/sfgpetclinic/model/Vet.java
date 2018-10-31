99b6d7b5b06792d96ca0987fd2b4b234ed68588c
Jasmin <jasmin.merusic@gmail.com>
2018-10-31 20:20:42 +0100

    added pets and contact info to owners on startup. closes #33

--- pet-clinic-data/src/main/java/com/jasko/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jasko/sfgpetclinic/model/Vet.java
@@ -1,15 +1,14 @@
 package com.jasko.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
-    private Set<Speciality> specialies;
+    private Set<Speciality> specialies=new HashSet<>();
 
     public Set<Speciality> getSpecialies() {
-
         return specialies;
-
     }
 
     public void setSpecialies(Set<Speciality> specialies) {
