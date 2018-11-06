476ef1ac1623682c85c4d1de0e68e07c78b1cac9
Jasmin <jasmin.merusic@gmail.com>
2018-11-06 10:28:13 +0100

    Implemented JPA services. closes #47

--- pet-clinic-data/src/main/java/com/jasko/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
+++ pet-clinic-data/src/main/java/com/jasko/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -3,12 +3,14 @@ package com.jasko.sfgpetclinic.services.springdatajpa;
 import com.jasko.sfgpetclinic.model.Speciality;
 import com.jasko.sfgpetclinic.repositories.SpecialityRepository;
 import com.jasko.sfgpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Profile("springdatajpa")
 public class SpecialitySDJpaService implements SpecialityService {
 
     private final SpecialityRepository specialityRepository;
